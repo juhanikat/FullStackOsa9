631de75efec8f35a10c16004e073a2646b0025d7
juhanikat <juhani.kataja@gmail.com>
2024-08-07 14:45:23 +0300

    done up to 9.15

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,19 +1,107 @@
+interface PartProps {
+  part: CoursePart;
+}
+
 interface HeaderProps {
   title: string;
 }
 
-interface coursePart {
-  name: string;
-  exerciseCount: number;
-}
-
 interface ContentProps {
-  courseParts: coursePart[];
+  courseParts: CoursePart[];
 }
 
 interface TotalProps {
   totalExercises: number;
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+interface CoursePartRequirements extends CoursePartDescription {
+  requirements: string[];
+  kind: "special";
+}
+
+type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartRequirements;
+
+const Part = (props: PartProps) => {
+  const part = props.part;
+  let returned;
+  switch (part.kind) {
+    case "basic":
+      returned = (
+        <>
+          <h2>
+            {part.name} {part.exerciseCount}
+          </h2>
+          <p>{part.description}</p>
+        </>
+      );
+      break;
+    case "group":
+      returned = (
+        <>
+          <h2>
+            {part.name} {part.exerciseCount}
+          </h2>
+          <p>Group project exercises {part.groupProjectCount}</p>
+        </>
+      );
+      break;
+    case "background":
+      returned = (
+        <>
+          <h2>
+            {part.name} {part.exerciseCount}
+          </h2>
+          <p>{part.description}</p>
+          <p>Background material: {part.backgroundMaterial}</p>
+        </>
+      );
+      break;
+    case "special":
+      returned = (
+        <>
+          <h2>
+            {part.name} {part.exerciseCount}
+          </h2>
+          <p>{part.description}</p>
+          <p>Required Skills: </p>
+          {part.requirements.map((part) => (
+            <p>{part}</p>
+          ))}
+        </>
+      );
+      break;
+    default:
+      break;
+  }
+  return returned;
+};
 
 const Header = (props: HeaderProps) => {
   return (
@@ -24,18 +112,12 @@ const Header = (props: HeaderProps) => {
 };
 
 const Content = (props: ContentProps) => {
-  const courseParts = props.courseParts;
+  const courseParts: CoursePart[] = props.courseParts;
   return (
     <div>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
+      {courseParts.map((part) => (
+        <Part part={part} />
+      ))}
     </div>
   );
 };
@@ -50,18 +132,45 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special",
     },
   ];
 
